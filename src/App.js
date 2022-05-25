0e751914219ec38ba4d42cd64ca76eea81465cbc
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-25 19:58:16 -0400

    render first and second components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // Import data
 
 // Import components
+import { Searchbar, searchbar } from './searchBar.js'
+import { Gallery } from './gallery';
 import './App.css';
 
 function App(){
   return (
     <div>
-      <h1>craigslist</h1>
+      <h1>Craigslist</h1>
+      <Searchbar />
+      <Gallery />
       <div className="App">
         {/* Your content will go here! */}
       </div>
---
+++ src/searchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Searchbar () {
+    return (
+        <h1>Search Bar</h1>
+    )
+}
+
+export {Searchbar}
